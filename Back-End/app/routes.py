d6ed4d4bc591e48e54c96de5c7c7b2e5bbe0cdcf
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-08-05 00:00:15 -0700

    get status of my plant function created in the back end

--- Back-End/app/routes.py
+++ Back-End/app/routes.py
@@ -7,7 +7,7 @@ from app import db
 from dotenv import load_dotenv
 from .models.user_device import User_Device
 from .models.device_reading import Device_Reading
-# import requests
+import random
 
 
 user_device_bp = Blueprint("users_devices", __name__, url_prefix="/user_devices")
@@ -81,32 +81,39 @@ def invalid_device_reading_post_request_body(request_body):
 
 @device_reading_bp.route("/<string:device_id>/all", methods=["GET"])
 def get_all_device_readings(device_id):
-    # device_reading = Device_Reading.query.get_or_404(device_id, "Incorrect device id")
     device_reading_list = [device_reading.to_json() for device_reading in Device_Reading.query.filter_by(device_id=device_id).all()]
     if device_reading_list:
         return jsonify(device_reading_list), 200
     else:
-        #Device_id may not exist
         return make_response({"error": "incorrect device_id or no readings for this device"}, 400)
 
 
-
-@device_reading_bp.route("/<string:device_id>/last", methods=["GET"])
-def get_last_device_reading(device_id):
-    current_time = datetime.now()
-    twentyfour_hours_ago = current_time - timedelta(days=1)
-
-    device_reading_list = [device_reading.to_json() for device_reading in Device_Reading.query.filter(device_id==device_id, Device_Reading.reading_time >= twentyfour_hours_ago).order_by(desc(Device_Reading.reading_time)).limit(1)]
-    if device_reading_list:
-        return jsonify(device_reading_list), 200
+@device_reading_bp.route("/<string:device_id>", methods=["GET"])
+def get_status_of_my_plant(device_id):
+    last_device_reading = get_reading_from_last_twentyfour_hours(device_id)
+    if last_device_reading:
+        return check_status(last_device_reading.to_json())
     else:
         #TODO: Call the raspberry pi to access real time data
         real_time_reading = get_real_time_device_reading()
         if real_time_reading:
-            return jsonify(real_time_reading), 200
+            return check_status(jsonify(real_time_reading))
         else:
-            # If the device cannot be reached, get the last record from the database 
-            return get_last_reading_stored(device_id)
+            # If the device cannot be reached, then get the last record from the database
+            last_reading_stored =  get_last_reading_stored(device_id)
+            if last_reading_stored:
+                return check_status(last_reading_stored.to_json())
+            else:
+                #TODO check how to send this to Alexa_Lambda
+                return make_response({"error": "Your plant is lost in the cloud. Check how to send this message to Alexa"}, 400)
+
+
+
+def get_reading_from_last_twentyfour_hours(device_id):
+    current_time = datetime.now()
+    twentyfour_hours_ago = current_time - timedelta(days=1)
+    last_device_reading = Device_Reading.query.filter(device_id==device_id, Device_Reading.reading_time >= twentyfour_hours_ago).order_by(desc(Device_Reading.reading_time)).limit(1).first()
+    return last_device_reading
 
 
 def get_real_time_device_reading():
@@ -114,9 +121,32 @@ def get_real_time_device_reading():
 
 
 def get_last_reading_stored(device_id):
-    device_reading_list = [device_reading.to_json() for device_reading in Device_Reading.query.filter_by(device_id=device_id).order_by(desc(Device_Reading.reading_time)).limit(1)]
-    if device_reading_list:
-        return jsonify(device_reading_list), 200
+    last_reading_stored = Device_Reading.query.filter_by(device_id=device_id).order_by(desc(Device_Reading.reading_time)).limit(1).first()
+    return last_reading_stored
+
+
+def check_status(response):
+    if int(response["moisture_sensor"]) < 30:
+        return make_response({"Status": "Your plant needs more water and love"}, 200)
+    elif int(response["moisture_sensor"]) > 50:
+        return make_response({"Status": "No more water for your plant. You will drown it."}, 200)
+    elif int(response["temperature_sensor"]) < 65:
+        return make_response({"Status": "Your plant needs to feel cozy. Move it to a warmer place"}, 200)
+    elif int(response["temperature_sensor"]) > 80:
+        return make_response({"Status": "With this high temperature, it will not grow. Move it to a cooler place"}, 200)
+    elif int(response["light_sensor"]) < 100:
+        return make_response({"Status": "This beauty needs more light"}, 200)
+    elif int(response["light_sensor"]) > 250:
+        return make_response({"Status": "Too much light! Some shade would be great"}, 200)
     else:
-        #TODO check how to send this to Alexa_Lambda
-        return make_response({"error": "incorrect device_id or no readings for this device. Check how to send this message to Alexa"}, 400)
+        return make_response(ok_status_responses(), 200)
+
+
+def ok_status_responses():
+    my_plant_status = [
+        {"Status" : "You are doing a great job with this plant. It is doing great!"}, 
+        {"Status" : "Your plant loves you! It is in great shape."}, 
+        {"Status" : "Your plant looks marvellous. Great job!"}, 
+        {"Status" : "You were born for this. Your plant is doing great!"}
+    ]
+    return my_plant_status[random.randint(0,len(my_plant_status)-1)]
